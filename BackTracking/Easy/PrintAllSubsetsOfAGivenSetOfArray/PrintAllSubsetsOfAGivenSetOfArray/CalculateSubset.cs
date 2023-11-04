c251109680b575a79b20bdd9765a2c6d51b161a6
Tauqeer Murtaza <tauqeer.287@hotmail.com>
2023-11-04 14:44:45 -0500

    Adding solution for Print All Subsets Of A Given Set Of Array

---
+++ BackTracking/Easy/PrintAllSubsetsOfAGivenSetOfArray/PrintAllSubsetsOfAGivenSetOfArray/CalculateSubset.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrintAllSubsetsOfAGivenSetOfArray
+{
+    public class CalculateSubset
+    {
+        public IList<IList<int>> SubSet(IList<int> arr)
+        {
+            List<int> subset = new List<int>();
+            List<IList<int>> res = new List<IList<int>>();
+            int index = 0;
+            CalcSubset(arr, res, subset, index);
+            return res;
+        }
+
+        private void CalcSubset(IList<int> arr, IList<IList<int>> res, List<int> subset, int index)
+        {
+            //Add the current to the result list
+            res.Add(new List<int>(subset));
+
+            //Generate subsets by recursively including and excluding elements
+            for (int i = index; i < arr.Count; i++)
+            {
+                //Include the current element in the subset
+                subset.Add(arr[i]);
+                //Recursively generate subsets with the current element included
+                CalcSubset(arr, res, subset, i + 1);
+                //Exclude the current element with the current element included
+                subset.RemoveAt(subset.Count - 1);
+            }
+        }
+    }
+}
---
+++ BackTracking/Easy/PrintAllSubsetsOfAGivenSetOfArray/PrintAllSubsetsOfAGivenSetOfArray/Program.cs
@@ -0,0 +1,14 @@
+﻿/*
+ * INPUT: N=3, Arr=[1,2,3]
+ * OUTPUT: {}, {1},{1,2}, {1,3}, {2}, {2,3}, {3}, {1,2,3}
+ */
+
+using PrintAllSubsetsOfAGivenSetOfArray;
+
+CalculateSubset subset = new CalculateSubset();
+var arr = new List<int> { 1, 2, 3 };
+var res = subset.SubSet(arr);
+foreach (var item in res)
+{
+    Console.WriteLine(String.Join(" ", item));
+}
